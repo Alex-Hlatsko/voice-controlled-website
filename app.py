d71272990c1695b77b1d3cfc52b5329993f694bd
Alen-Berg <alexei.hlatsko@gmail.com>
2023-11-06 23:10:08 +0100

    voice-test

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import time
+import speech_recognition as sr
 
 app = Flask(__name__)
 
-voice_commands = ["open web", "show info", "hello"]
+recognizer = sr.Recognizer()
+
+voice_commands = ["открой сайт", "покажи информацию", "привет"]
 
 @app.route('/')
 def index():
     return render_template('index.html', commands=voice_commands)
+
+@app.route('/listen')
+def listen():
+    return render_template('listen.html')
+
+@app.route('/listen_now', methods=['GET', 'POST'])
+def listen_now():
+    timer = 10
+    audio = None
+    with sr.Microphone() as source:
+        print("Скажите что-нибудь...")
+        for i in range(timer):
+            audio = recognizer.listen(source, timeout=1)
+            if audio:
+                break
+
+    if audio:
+        try:
+            command = recognizer.recognize_google(audio, language="ru-RU")
+            print(f"Вы сказали: {command}")
+            return jsonify({"result": f"Вы сказали: {command}"})
+        except sr.UnknownValueError:
+            return jsonify({"error": "Речь не распознана"})
+        except sr.RequestError as e:
+            return jsonify({"error": f"Ошибка при запросе к сервису распознавания: {e}"})
+    else:
+        return jsonify({"error": "Тайм-аут распознавания"})
 
 if __name__ == '__main__':
     app.run(debug=True)
